522f57e6c32aeba342e7fa8a940fe6570b659634
Svyatoslav Ilinskiy <ilinskiy.sv@gmail.com>
2015-11-15 11:40:39 +0300

    refactor tests

--- test/me/ilinskiy/ChessAI/AITest.scala
+++ test/me/ilinskiy/ChessAI/AITest.scala
@@ -10,14 +10,15 @@ import org.junit.Assert
   * Created by ilinum on 08.11.15.
   */
 class AITest extends TestCase {
-
   def testEatPawn(): Unit = {
     val board = new BoardWrapper()
     board.setPieceAt((0, 3), Piece.createPiece(PieceColor.White, PieceType.Pawn))
     board.setPieceAt((1, 2), Piece.createPiece(PieceColor.Black, PieceType.Pawn))
-    val whiteMove = MoveMaker.getMove(board, PieceColor.White, None)
-    Assert.assertEquals(new Move((0, 3),(1, 2)), whiteMove)
+    doTest(board.getInner, new Move((0, 3), (1, 2)))
   }
 
-
+  def doTest(board: Board, expected: Move): Unit = {
+    val ai = new AI(board.whoseTurnIsIt())
+    Assert.assertEquals(expected, ai.getMove(board))
+  }
 }
